7d562b3d533cf88e49dc46b9011b17cac90f6176
Johnathon Wood <jaywood128@gmail.com>
2019-02-20 23:14:16 +0000

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -2,28 +2,35 @@ class OwnersController < ApplicationController
 
   get '/owners' do
     @owners = Owner.all
-    erb :'/owners/index' 
+    erb :'/owners/index'
   end
 
-  get '/owners/new' do 
-    erb :'/owners/new'
+  get '/owners/new' do
+    @pets = Pet.all
+   erb :'/owners/new'
   end
 
-  post '/owners' do 
-    
+  post '/owners' do
+
+    @owner = Owner.create(params["owner"])
+    @pet = Pet.create(params["pet"])
+    @owner.pets << @pet
+    @owner.pets
+
+    redirect "owners/#{@owner.id}"
   end
 
-  get '/owners/:id/edit' do 
+  get '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
     erb :'/owners/edit'
   end
 
-  get '/owners/:id' do 
+  get '/owners/:id' do
     @owner = Owner.find(params[:id])
     erb :'/owners/show'
   end
 
-  patch '/owners/:id' do 
-   
+  patch '/owners/:id' do
+
   end
 end
